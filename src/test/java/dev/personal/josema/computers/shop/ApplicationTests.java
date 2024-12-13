c5f8afb52070eb69ab6251434a383a8681e040d6
jose30-back <josema.bootcamp@gmail.com>
2024-12-13 16:35:35 +0100

    Initial Spring Boot project setup

---
+++ src/test/java/dev/personal/josema/computers/shop/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.personal.josema.computers.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
